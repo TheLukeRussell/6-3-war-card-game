a7448fca0c55a093d4cfa8dddccc3ee0f0665e4c
Luke Russell <thelukerussel21@gmail.com>
2020-02-12 15:46:43 -0500

    randomizes deck and can show which player wins

--- game.py
+++ game.py
@@ -1,161 +1,35 @@
 import random
-card = random.randint(1,52)
-for i in range(100):
-    if (card == 1): 
-        card = "Ace of Spades"
-        print(card)
-    elif (card == 2):
-        card = "Two of Spades"
-        print(card)
-    elif (card == 3):
-        card = "Three of Spades"
-        print(card)
-    elif (card == 4):
-        card = "Four of Spades"
-        print(card)
-    elif (card == 5):
-        card = "Five of Spades"
-        print(card)
-    elif (card == 6):
-        card = "Six of Spades"
-        print(card)
-    elif (card == 7):
-        card = "Seven of Spades"
-        print(card)
-    elif (card == 8):
-        card = "Eight of Spades"
-        print(card)
-    elif (card == 9):
-        card = "Nine of Spades"
-        print(card)
-    elif (card == 10):
-        card = "Ten of Spades"
-        print(card)
-    elif (card == 11):
-        card = "Jack of Spades"
-        print(card)
-    elif (card == 12):
-        card = "Queen of Spades"
-        print(card)
-    elif (card == 13):
-        card = "King of Spades"
-        print(card)
-    elif (card == 14): 
-        card = "Ace of Clubs"
-        print(card)
-    elif (card == 15):
-        card = "Two of Clubs"
-        print(card)
-    elif (card == 16):
-        card = "Three of Clubs"
-        print(card)
-    elif (card == 17):
-        card = "Four of Clubs"
-        print(card)
-    elif (card == 18):
-        card = "Five of Clubs"
-        print(card)
-    elif (card == 19):
-        card = "Six of Clubs"
-        print(card)
-    elif (card == 20):
-        card = "Seven of Clubs"
-        print(card)
-    elif (card == 21):
-        card = "Eight of Clubs"
-        print(card)
-    elif (card == 22):
-        card = "Nine of Clubs"
-        print(card)
-    elif (card == 23):
-        card = "Ten of Clubs"
-        print(card)
-    elif (card == 24):
-        card = "Jack of Clubs"
-        print(card)
-    elif (card == 25):
-        card = "Queen of Clubs"
-        print(card)
-    elif (card == 26):
-        card = "King of Clubs"
-        print(card)
-    elif (card == 27): 
-        card = "Ace of Diamonds"
-        print(card)
-    elif (card == 28):
-        card = "Two of Diamonds"
-        print(card)
-    elif (card == 29):
-        card = "Three of Diamonds"
-        print(card)
-    elif (card == 30):
-        card = "Four of Diamonds"
-        print(card)
-    elif (card == 31):
-        card = "Five of Diamonds"
-        print(card)
-    elif (card == 32):
-        card = "Six of Diamonds"
-        print(card)
-    elif (card == 33):
-        card = "Seven of Diamonds"
-        print(card)
-    elif (card == 34):
-        card = "Eight of Diamonds"
-        print(card)
-    elif (card == 35):
-        card = "Nine of Diamonds"
-        print(card)
-    elif (card == 36):
-        card = "Ten of Diamonds"
-        print(card)
-    elif (card == 37):
-        card = "Jack of Diamonds"
-        print(card)
-    elif (card == 38):
-        card = "Queen of Diamonds"
-        print(card)
-    elif (card == 39):
-        card = "King of Diamonds"
-        print(card)
-    elif (card == 40): 
-        card = "Ace of Hearts"
-        print(card)
-    elif (card == 41):
-        card = "Two of Hearts"
-        print(card)
-    elif (card == 42):
-        card = "Three of Hearts"
-        print(card)
-    elif (card == 43):
-        card = "Four of Hearts"
-        print(card)
-    elif (card == 44):
-        card = "Five of Hearts"
-        print(card)
-    elif (card == 45):
-        card = "Six of Hearts"
-        print(card)
-    elif (card == 46):
-        card = "Seven of Hearts"
-        print(card)
-    elif (card == 47):
-        card = "Eight of Hearts"
-        print(card)
-    elif (card == 48):
-        card = "Nine of Hearts"
-        print(card)
-    elif (card == 49):
-        card = "Ten of Hearts"
-        print(card)
-    elif (card == 50):
-        card = "Jack of Hearts"
-        print(card)
-    elif (card == 51):
-        card = "Queen of Hearts"
-        print(card)
-    elif (card == 52):
-        card = "King of Hearts"
-        print(card)
+
+
+
+def shuffleDeck():
+    global deck
+    deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']*4
+
+    random.shuffle(deck)
+shuffleDeck()
+
+def playWar():
+    hand_1 = deck[:26]
+    hand_2 = deck[26:]
+
+    pile_1 = []
+    pile_2 = []
+
+    card_value = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+    round = 1
+
+    while hand_1 and hand_2:
+        player_1 = hand_1.pop()
+        player_2 = hand_2.pop()
         
-    
+        if player_1 == player_2:
+            print('The cards are equal')
+
+        elif player_1 > player_2:
+            print('Player1 wins')
+
+        elif player_1 < player_2:
+            print('Player2 wins')
+
+playWar()
